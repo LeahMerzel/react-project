36011c4d4653e14b5e3c49f1913511388c695bf9
LeahMerzel <leahwalden4@gmail.com>
2024-11-08 10:57:26 +0200

    subheading added

--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -10,22 +10,36 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <Accordion defaultActiveKey="0" flush>
-        {Object.entries(topics).map(([groupName, subtopics], index) => (
-          <Accordion.Item eventKey={index.toString()} key={index}>
+        {Object.entries(topics).map(([groupName, subheadings], index) => (
+          <Accordion.Item eventKey={`group-${index}`} key={`group-${index}`}>
             <Accordion.Header>{groupName}</Accordion.Header>
             <Accordion.Body>
-              <Nav className="flex-column">
-                {subtopics.map((topic, subIndex) => (
-                  <Nav.Link
-                    as={Link}
-                    to={topic.path}
-                    key={subIndex}
-                    className="pl-3"
-                  >
-                    {topic.name}
-                  </Nav.Link>
-                ))}
-              </Nav>
+              <Accordion>
+                {Object.entries(subheadings).map(
+                  ([subheadingName, topicsArray], subIndex) => (
+                    <Accordion.Item
+                      eventKey={`sub-${index}-${subIndex}`}
+                      key={`sub-${index}-${subIndex}`}
+                    >
+                      <Accordion.Header>{subheadingName}</Accordion.Header>
+                      <Accordion.Body>
+                        <Nav className="flex-column">
+                          {topicsArray.map((topic, topicIndex) => (
+                            <Nav.Link
+                              as={Link}
+                              to={topic.path}
+                              key={`topic-${index}-${subIndex}-${topicIndex}`}
+                              className="pl-3"
+                            >
+                              {topic.name}
+                            </Nav.Link>
+                          ))}
+                        </Nav>
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  )
+                )}
+              </Accordion>
             </Accordion.Body>
           </Accordion.Item>
         ))}
